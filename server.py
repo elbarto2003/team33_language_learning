87bc42bdbf49dd4345b7c92f2001deabea420fec
Alvin-G16 <144556755+Alvin-G16@users.noreply.github.com>
2024-11-02 17:11:43 +0000

    added auth0 basics

---
+++ server.py
@@ -0,0 +1,7 @@
+import json
+from os import environ as env
+from urllib.parse import quote_plus, urlencode
+
+from authlib.integrations.flask_client import OAuth
+from dotenv import find_dotenv, load_dotenv
+from flask import Flask, redirect, render_template, session, url_for
